f27b01f89eea26d35755a86563b92358a00d2a1b
辻昌紘 <tsujiakihiro@tsujimayuminoMacBook-puro.local>
2019-01-22 13:55:00 +0900

    WebProgramming

--- Project/src/contoroller/userManagementServlet.java
+++ Project/src/contoroller/userManagementServlet.java
@@ -9,6 +9,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import model.User;
 /**
  * Servlet implementation class userManagementServlet
  */
@@ -35,9 +39,39 @@ public class userManagementServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        // リクエストパラメータの文字コードを指定
+        request.setCharacterEncoding("UTF-8");
+
+		// リクエストパラメータの入力項目を取得
+		String loginId = request.getParameter("loginId");
+		String password = request.getParameter("password");
+
+		// リクエストパラメータの入力項目を引数に渡して、Daoのメソッドを実行
+		UserDao userDao = new UserDao();
+		User user = userDao.findByLoginInfo(loginId, password);
+
 	}
+
+	/** テーブルに該当のデータが見つからなかった場合 **/
+	if (user == null) {
+		// リクエストスコープにエラーメッセージをセット
+		request.setAttribute("errMsg", "ログインに失敗しました。");
+
+		// ログインjspにフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/UserManagement.jsp");
+		dispatcher.forward(request, response);
+		return;
+	}
+	/** テーブルに該当のデータが見つかった場合 **/
+	// セッションにユーザの情報をセット
+	HttpSession session = request.getSession();
+	session.setAttribute("userInfo", user);
+
+	// ユーザ一覧のサーブレットにリダイレクト
+	response.sendRedirect("UserListServlet");
+
+}
 
 }
